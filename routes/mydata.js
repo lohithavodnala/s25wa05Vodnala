c471917f8f1d9f6a6da80af220fb3113973c7c67
lohithavodnala <vodnalalohitha@gmail.com>
2025-03-26 12:22:37 -0500

    added mydata in routers and updated app.js and update mydata.js

---
+++ routes/mydata.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET mydata page. */
+router.get('/', function(req, res, next) {
+  res.render('mydata', { title: 'lohitha vodnala' });
+});
+
+module.exports = router;
+
